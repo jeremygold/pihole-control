762cf166d08804dc8d2a0cc5063bdc37134ec09f
Jeremy Gold <jeremy.gold@ikegps.com>
2017-07-04 00:36:21 +1200

    Updating to remove dependance on jquery

--- routes/control.js
+++ routes/control.js
@@ -3,15 +3,14 @@ var router = express.Router();
 
 /* Enable pihole. */
 router.get('/enable', function(req, res, next) {
-    console.log("Enabling pihole");
     global.stat = "Enabled";
     res.send(global.stat);
 });
 
 /* Disable pihole */
 router.get('/disable', function(req, res, next) {
-    console.log("Disabling pihole");
     global.stat = "Disabled";
+    //res.setHeader('content-type', 'text/plain');
     res.send(global.stat);
 });
 
