5a1f84bd3a24a44c047cb20280e338aedfa6475d
Jeremy Gold <jeremy.gold@ikegps.com>
2017-07-05 22:36:32 +1200

    Initial implementation of firebase pihole-control... WIP

--- routes/control.js
+++ routes/control.js
@@ -1,5 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var firebase = require("firebase/app");
+require("firebase/auth");
+require("firebase/database");
+
+// Initialize Firebase
+var config = {
+		apiKey: "AIzaSyD7BweyD6hJYW4NqTULbxw0MdyoWg6jftg",
+		authDomain: "pihole-control.firebaseapp.com",
+		databaseURL: "https://pihole-control.firebaseio.com",
+		projectId: "pihole-control",
+		storageBucket: "pihole-control.appspot.com",
+		messagingSenderId: "210194800964"
+};
+firebase.initializeApp(config);
 
 /* Enable pihole. */
 router.get('/enable', function(req, res, next) {
