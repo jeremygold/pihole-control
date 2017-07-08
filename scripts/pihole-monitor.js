b791def33f51733e8f7d67995d1de554390d99dd
Jeremy Gold <jeremy.gold@ikegps.com>
2017-07-08 23:00:40 +1200

    moving scripts in to scripts folder, and getting initial call into shell script working

--- scripts/pihole-monitor.js
+++ scripts/pihole-monitor.js
@@ -1,14 +1,20 @@
 #!/usr/bin/node
 
+var exec = require('child_process').exec;
 var admin = require("firebase-admin");
 var serviceAccount = require("./pihole-control-private-key.json");
 
 const stateRef = 'pihole-state';
 
+function log(error, stdout, stderr) { 
+    console.log("stdout: " + stdout);
+}
+
 function addDatabaseCallback() {
     console.log("Adding Database Callback");
     admin.database().ref(stateRef).on('value', function(snapshot) {
         console.log("Dbase change detected - Status is now " + snapshot.val().stat);
+        exec("./set-config.sh " + snapshot.val().stat, log);
     });
 }
 
---
+++ scripts/set-config.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# pihole_cmd=/usr/local/bin/pihole
+# config_file=/etc/dnsmasq.d/03-pihole-wildcard.conf
+# temp_location=/etc/03-pihole-wildcard.conf
+
+# Temp variables for testing
+pihole_cmd=echo
+config_file=/home/jeremy/test.config
+temp_location=/home/jeremy/temp/test.config
+
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root" 1>&2
+    exit 1
+fi
+
+if [ "$1" != "" ]; then
+    pihole_status="$1"
+    echo "Pihole status: $pihole_status"
+
+    if [[ $pihole_status == "Enabled" ]]
+    then
+        if [ -f $temp_location ]
+        then
+            echo "Enabling pihole"
+            mv -v $temp_location $config_file
+            $pihole_cmd restartdns
+        else
+            echo "Pihole already enabled"
+        fi
+    else
+        if [ -f $config_file ]
+        then
+            echo "Disabling pihole"
+            mv -v $config_file $temp_location
+            $pihole_cmd restartdns
+        else
+            echo "Pihole already disabled"
+        fi
+    fi
+fi
